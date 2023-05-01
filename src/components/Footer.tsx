899c6f705a49850b463c5f7f3c02da48557401f8
drxid <dr.register@mail.ru>
2023-05-01 19:47:28 +0300

    setting up react + ts + vite, tw & prettier + eslint

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer: React.FC = () => {
+  return (
+    <footer>
+      {/* Код подвала */}
+    </footer>
+  )
+}
+
+export default Footer;
---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header: React.FC = () => {
+  return (
+    <header>
+      {/* Код подвала */}
+    </header>
+  )
+}
+
+export default Header
